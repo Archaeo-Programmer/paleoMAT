cd109091f282be0598969061f18c9db016731edf
Andrew Gillreath-Brown <andrew.brown1234@gmail.com>
2020-08-26 10:39:41 -0700

    Added a wn in front of URL for get_coreTops function. Also, updated Nafossil_final in Rmd to cleanup and get rid of negative values.

--- R/get_coreTops.R
+++ R/get_coreTops.R
@@ -4,7 +4,8 @@ get_coreTops <- function(){
   # I am using a service online ( https://tilia.neotomadb.org/retrieve/doc2/), which allows for me
   # to target the core tops through the SKOPE_GetSurfaceSampleData method (use selection #3 for core-top samples; 7 for surface samples).
   # Then, I copied the json file and used an online service to convert the json file to a csv, which I am reading in here.
-  httr::GET("https://tilia.neotomadb.org/retrieve/",
+  # Note: had to add a "wn" in front of tilia.neotomadb.org as the URL changed probably due to the transition to PostgreSQL.
+  httr::GET("https://wntilia.neotomadb.org/retrieve/",
             query = list(method = "SKOPE_GetSurfaceSampleData",
                          DATASETTYPEID = 3,
                          EASTLONGBOUND = -100)) %>%
