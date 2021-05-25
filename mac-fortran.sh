0924e03e20755f922d280aae3e565a75b1fac579
Andrew Gillreath-Brown <andrew.brown1234@gmail.com>
2021-05-25 16:39:00 -0700

    Update to temporal interpolation.

---
+++ mac-fortran.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+echo "downloading gfortran-4.8.2 and unpacking it"
+
+mkdir -p ~/gfortran
+curl -OL https://mac.r-project.org/libs/gfortran-4.8.2-darwin13.tar.bz2
+tar -C ~/gfortran -jxvf gfortran-4.8.2-darwin13.tar.bz2
+
+echo "cleaning up: removing tarball"
+rm -f gfortran-4.8.2-darwin13.tar.bz2
+
+echo "updating Makevars file in ~/.R to tell R about the new fortran"
+mkdir -p ~/.R
+cat >> ~/.R/Makevars <<EOF
+F77 = ~/gfortran/usr/local/bin/gfortran
+FC = ~/gfortran/usr/local/bin/gfortran
+FLIBS = -L ~/gfortran/usr/local/lib
+EOF
