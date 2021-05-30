5707096ebe5ca1694b226afcdab11e940277a222
Andrew Gillreath-Brown <andrew.brown1234@gmail.com>
2021-05-29 17:06:18 -0700

    Updated interpolate_time function to use mgcv::gam rather than the previous method.

--- R/interpolate_time.R
+++ R/interpolate_time.R
@@ -1,37 +1,29 @@
 interpolate_time <-
   function(temp_data) {
 
-    Q <- quantile(temp_data$anom, probs=c(.25, .75), na.rm = FALSE)
+    Q <- quantile(temp_data$anom, probs=c(.01, .99), na.rm = FALSE)
     iqr <- IQR(temp_data$anom)
     up <-  Q[2]+1.5*iqr # Upper Range
     low<- Q[1]-1.5*iqr # Lower Range
     eliminated <- subset(temp_data, temp_data$anom > (Q[1] - 1.5*iqr) & temp_data$anom < (Q[2]+1.5*iqr))
 
-    fit <- spline(eliminated$date, eliminated$anom, method = "natural")
-    fit <- as.data.frame(fit)
+    gam_fit <- mgcv::gam(anom ~ s(date, bs = 'cs', k = (length(eliminated$anom)/2)), data = eliminated, family=gaussian())
+    gam_knots <- gam_fit$smooth[[1]]$xp  ## extract knots locations
 
-    Q <- quantile(fit$y, probs=c(.25, .75), na.rm = FALSE)
-    iqr <- IQR(fit$y)
-    up <-  Q[2]+1.5*iqr # Upper Range
-    low<- Q[1]-1.5*iqr # Lower Range
-    eliminated <- subset(fit, fit$y > (Q[1] - 1.5*iqr) & fit$y < (Q[2]+1.5*iqr))
-
-    fit <- smooth.spline(eliminated, all.knots = TRUE)
-
-    agemax <- max(eliminated$x)
+    agemax <- max(eliminated$date)
     agemax_rounded <- agemax %>%
       DescTools::RoundTo(multiple = 100, FUN = ceiling)
-    agemin <- min(eliminated$x)
+    agemin <- min(eliminated$date)
     agemin_rounded <- agemin %>%
       DescTools::RoundTo(multiple = 100, FUN = floor)
 
-    midpoints <- seq((agemin_rounded + 50), (agemax_rounded - 50), by = 100)
-    fit <- as.data.frame(predict(fit, agemin:agemax)) %>%
-      dplyr::filter(x >= -1000 & x <= 1799) %>%
-      group_by(mean = (row_number() -1) %/% 100) %>%
-      mutate(mean = mean(y)) %>%
-      dplyr::filter(x %in% midpoints) %>%
-      dplyr::select(-y)
+    predict.points <- as.data.frame(seq(agemin_rounded, agemax_rounded, by = 100))
+    #predict.points <- as.data.frame(seq((agemin_rounded + 50), (agemax_rounded - 50), by = 100))
+    names(predict.points)[1] <- "date"
+
+    predict.anom <- mgcv::predict.gam(gam_fit,predict.points,type='response')
+    fit <- as.data.frame(predict.points,anom=predict.anom) %>%
+      dplyr::filter(x >= -1000 & x <= 1800)
 
     return(fit)
 
