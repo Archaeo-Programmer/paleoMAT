6d18ef88d88d4ca2b6f739ef40d24e3d52160fea
Andrew Gillreath-Brown <andrew.brown1234@gmail.com>
2021-05-31 18:00:41 -0700

    Formatted temperature predictions so that the data would be prepared for 100 year increments. Updated map_predictions function.

--- R/map_predictions.R
+++ R/map_predictions.R
@@ -1,23 +1,51 @@
 map_predictions <-
-  function(site.preds, prism.raster) {
-
-    m_int <- mgcv::gam(anom ~s(long, lat), data = site.preds)
-    rsst2 <- aggregate(prism.raster, 2)
+  function(site.preds, prism.raster, preds.extent) {
+    # First, prepare the raster data, so that the prediction can be done on the raster grid.
+    # The PRISM raster data is at a very fine resolution, so the size of raster cells are increased slightly.
+    rtemp <- raster::aggregate(prism.raster, fact = 2)
     icell <- 1:ncell(prism.raster)
-    pred <- data.frame(value = rsst2[icell],
-                       cells = icell,
-                       x = xFromCell(rsst2, icell),
-                       y = yFromCell(rsst2, icell))
+    pred <- data.frame(
+      value = rtemp[icell],
+      cells = icell,
+      x = xFromCell(rtemp, icell),
+      y = yFromCell(rtemp, icell)
+    )
     pred <- na.omit(pred)
 
     pred <- pred %>%
       dplyr::mutate(value = value / 10.00)
 
     names(pred)[3:4] <- c("long", "lat")
-    pred$anom_pred <- predict(m_int, newdata = pred, type = "response")
 
-    rpred <- raster(rsst2)
-    rpred[pred$cells] <- pred$richness_pred
-    rpred_cropped <- crop(x = rpred, y = bb2)
+    # Next, get the number of sites as mgcv::gam will throw an error message if the number of samples is too small (i.e., less than 30).
+    # However, this can be circumvented by assigning a k value for those less than 30. Here, the total number of sites is used when the
+    # number of sites is less than 30. If equal to or greater than 30, then the model runs with the default settings.
+    no.sites <- nrow(site.preds)
+    # no.sites <- length(site.preds[[1]][[1]])
+
+    if (no.sites < 30) {
+      #no.samples = no.sites
+      m_int <-
+        mgcv::gam(anom ~ s(long, lat, k = no.sites), data = site.preds)
+
+      pred$anom_pred <-
+        predict(m_int, newdata = pred, type = "response")
+
+      rpred <- raster(rtemp)
+      rpred[pred$cells] <- pred$anom_pred
+      rpred_cropped <- raster::crop(x = rpred, y = preds.extent)
+
+
+    } else {
+      m_int <- mgcv::gam(anom ~ s(long, lat), data = site.preds)
+
+      pred$anom_pred <-
+        predict(m_int, newdata = pred, type = "response")
+
+      rpred <- raster(rtemp)
+      rpred[pred$cells] <- pred$anom_pred
+      rpred_cropped <- raster::crop(x = rpred, y = preds.extent)
+
+    }
 
   }
