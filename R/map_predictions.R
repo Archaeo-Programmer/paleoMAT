e816ac0bb171ca7123678ed5b35d8f9dbb0ffe18
Andrew Gillreath-Brown <andrew.brown1234@gmail.com>
2021-06-01 01:00:04 -0700

    Finished writing code for the map_predictions function and the workflow of applying this function in the UUSS_MAT_Reconstruction.Rmd.

--- R/map_predictions.R
+++ R/map_predictions.R
@@ -12,29 +12,32 @@ map_predictions <-
     )
     pred <- na.omit(pred)
 
-    pred <- pred %>%
-      dplyr::mutate(value = value / 10.00)
+    # pred <- pred %>%
+    #   dplyr::mutate(value = value / 10.00)
 
     names(pred)[3:4] <- c("long", "lat")
 
     # Next, get the number of sites as mgcv::gam will throw an error message if the number of samples is too small (i.e., less than 30).
-    # However, this can be circumvented by assigning a k value for those less than 30. Here, the total number of sites is used when the
-    # number of sites is less than 30. If equal to or greater than 30, then the model runs with the default settings.
+    # However, this can be circumvented by assigning a value for k for those time periods that are less than 30.
+    # Here, the total number of sites is used when the number of sites is less than 30. If equal to or greater than 30, then the model
+    # runs with the default settings.
     no.sites <- nrow(site.preds)
     # no.sites <- length(site.preds[[1]][[1]])
 
     if (no.sites < 30) {
-      #no.samples = no.sites
+      # For time periods with less than 30 sites, the total number of sites/samples are used for k.
       m_int <-
         mgcv::gam(anom ~ s(long, lat, k = no.sites), data = site.preds)
 
+      #pred$elev <- 0
       pred$anom_pred <-
         predict(m_int, newdata = pred, type = "response")
 
-      rpred <- raster(rtemp)
+      rpred <- raster::raster(rtemp)
       rpred[pred$cells] <- pred$anom_pred
       rpred_cropped <- raster::crop(x = rpred, y = preds.extent)
 
+      return(rpred_cropped)
 
     } else {
       m_int <- mgcv::gam(anom ~ s(long, lat), data = site.preds)
@@ -42,9 +45,11 @@ map_predictions <-
       pred$anom_pred <-
         predict(m_int, newdata = pred, type = "response")
 
-      rpred <- raster(rtemp)
+      rpred <- raster::raster(rtemp)
       rpred[pred$cells] <- pred$anom_pred
       rpred_cropped <- raster::crop(x = rpred, y = preds.extent)
+
+      return(rpred_cropped)
 
     }
 
