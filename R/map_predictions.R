235d3a864d2d3455de011b5da9f74f0dfbab92cd
Andrew Gillreath-Brown <andrew.brown1234@gmail.com>
2021-06-03 23:54:08 -0700

    Re-wrote the map_predictions function using thin plate spline regression rather than gam.

--- R/map_predictions.R
+++ R/map_predictions.R
@@ -1,56 +1,109 @@
 map_predictions <-
-  function(site.preds, prism.raster, preds.extent) {
-    # First, prepare the raster data, so that the prediction can be done on the raster grid.
-    # The PRISM raster data is at a very fine resolution, so the size of raster cells are increased slightly.
-    rtemp <- raster::aggregate(prism.raster, fact = 2)
-    icell <- 1:ncell(prism.raster)
-    pred <- data.frame(
-      value = rtemp[icell],
-      cells = icell,
-      x = xFromCell(rtemp, icell),
-      y = yFromCell(rtemp, icell)
-    )
-    pred <- na.omit(pred)
+  function(site.preds,
+           degree.res,
+           site.locs,
+           rast.extent = TRUE) {
+    if (rast.extent == TRUE) {
+      # First, prepare the raster grid, so that the interpolation can be done on the raster grid.
+      # Create a bounding box around the site extent (i.e., for site.locs).
+      bbox <- c(
+        "xmin" = min(site.locs$long),
+        "ymin" = min(site.locs$lat),
+        "xmax" = max(site.locs$long),
+        "ymax" = max(site.locs$lat)
+      )
 
-    # pred <- pred %>%
-    #   dplyr::mutate(value = value / 10.00)
+      grd_template <- expand.grid(
+        x = seq(
+          from = bbox["xmin"],
+          to = bbox["xmax"],
+          by = degree.res
+        ),
+        y = seq(
+          from = bbox["ymin"],
+          to = bbox["ymax"],
+          by = degree.res
+        ) # in degrees resolution
+      )
 
-    names(pred)[3:4] <- c("long", "lat")
+      #Rasterize the grid.
+      crs_raster_format <-
+        "+proj=utm +zone=19 +datum=NAD83 +units=m +no_defs"
 
-    # Next, get the number of sites as mgcv::gam will throw an error message if the number of samples is too small (i.e., less than 30).
-    # However, this can be circumvented by assigning a value for k for those time periods that are less than 30.
-    # Here, the total number of sites is used when the number of sites is less than 30. If equal to or greater than 30, then the model
-    # runs with the default settings.
-    no.sites <- nrow(site.preds)
-    # no.sites <- length(site.preds[[1]][[1]])
+      grd_template_raster <- grd_template %>%
+        dplyr::mutate(Z = 0) %>%
+        raster::rasterFromXYZ(crs = crs_raster_format)
 
-    if (no.sites < 30) {
-      # For time periods with less than 30 sites, the total number of sites/samples are used for k.
-      m_int <-
-        mgcv::gam(anom ~ s(long, lat, k = no.sites), data = site.preds)
-
-      #pred$elev <- 0
-      pred$anom_pred <-
-        predict(m_int, newdata = pred, type = "response")
-
-      rpred <- raster::raster(rtemp)
-      rpred[pred$cells] <- pred$anom_pred
-      rpred_cropped <- raster::crop(x = rpred, y = preds.extent)
-
-      return(rpred_cropped)
-
+      # If extent is true, then a bounding box is created from the extent of the sites.
+      # If it is false, then a polygon with a buffer is created.
     } else {
-      m_int <- mgcv::gam(anom ~ s(long, lat), data = site.preds)
+      # Get extent of fossil pollen sites and make a polygon.
+      fossilpnts <- site.locs %>%
+        sf::st_as_sf(coords = c("long", "lat"), crs = 4326)
+      fossilpolygon <- concaveman::concaveman(fossilpnts) %>%
+        sf::st_as_sf(crs = 4326) %>%
+        sf::st_transform(crs = 4326) %>%
+        sf:::as_Spatial()
 
-      pred$anom_pred <-
-        predict(m_int, newdata = pred, type = "response")
+      # Next, apply a buffer 15 mile (or 24140.2 meters) buffer from the extent of the sites (to be the edge of extrapolation from the sites). Here, the projection is changed as gBuffer (or GEOS) expects planar coordinates. Then, these are converted back in order to use mask to clip the raster below.
+      fossilpolygon <-
+        sp::spTransform(fossilpolygon,
+                        CRS("+proj=utm +zone=19 +datum=NAD83 +units=m +no_defs"))
+      fossilpolygon <- rgeos::gBuffer(fossilpolygon, width = 24140.2)
+      fossilpolygon <-
+        sp::spTransform(fossilpolygon, CRS("+init=epsg:4326"))
 
-      rpred <- raster::raster(rtemp)
-      rpred[pred$cells] <- pred$anom_pred
-      rpred_cropped <- raster::crop(x = rpred, y = preds.extent)
+      bbox <- c(
+        "xmin" = min(site.locs$long) - (degree.res * 4),
+        "ymin" = min(site.locs$lat) - (degree.res * 4),
+        "xmax" = max(site.locs$long) + (degree.res * 4),
+        "ymax" = max(site.locs$lat) + (degree.res * 4)
+      )
 
-      return(rpred_cropped)
+      grd_template <- expand.grid(
+        x = seq(
+          from = bbox["xmin"],
+          to = bbox["xmax"],
+          by = degree.res
+        ),
+        y = seq(
+          from = bbox["ymin"],
+          to = bbox["ymax"],
+          by = degree.res
+        ) # in degrees resolution
+      )
+
+      #Rasterize the grid.
+      crs_raster_format <-
+        "+proj=utm +zone=19 +datum=NAD83 +units=m +no_defs"
+
+      grd_template_raster <- grd_template %>%
+        dplyr::mutate(Z = 0) %>%
+        raster::rasterFromXYZ(crs = crs_raster_format)
+
+      grd_template_raster <-
+        raster::mask(grd_template_raster, fossilpolygon)
 
     }
+
+    # Next, get the number of sites, which is used to define the degrees of freedom (df).
+    no.sites <- nrow(site.preds)
+
+    # Fit the model as the first step in the process.
+    # Thin Plate Spline Regression
+    fit_TPS <- fields::Tps(
+      x = as.matrix(site.preds[, c("long", "lat")]),
+      # accepts points but expects them as matrix
+      Y = site.preds$anom,
+      # the dependent variable
+      #Z = ok$elev,
+      miles = TRUE,
+      df = no.sites * 0.6
+    )
+
+    interp_TPS <- interpolate(grd_template_raster, fit_TPS)
+    crs(interp_TPS) <- CRS('+init=EPSG:4326')
+
+    return(interp_TPS)
 
   }
