d5231853e210a2bde386fb92ec036dd40d3f4919
Andrew Gillreath-Brown <andrew.brown1234@gmail.com>
2021-10-25 15:05:52 -0700

    updated plot map anomaly to be able to exclude cities from map. Updated plotting the anomaly maps figure code.

--- R/plot_map_anomaly.R
+++ R/plot_map_anomaly.R
@@ -1,5 +1,5 @@
 plot_map_anomaly <-
-  function(x, x.name, us.cities, animation = FALSE) {
+  function(x, x.name, us.cities, cities = FALSE, animation = FALSE) {
     title.name <- x.name
     temperature <- x
 
@@ -29,10 +29,160 @@ plot_map_anomaly <-
     temperature <- as.data.frame(temperature, xy = TRUE)
     temperature_transformed <- usmap::usmap_transform(temperature)
 
+
+    if (cities == FALSE) {
+
+      anomaly_plots <- ggplot() +
+        geom_raster(
+          data = temperature_transformed,
+          aes(x = x, y = y, fill = var1.pred),
+          alpha = 0.8,
+          na.rm = TRUE
+        ) +
+        scale_fill_gradientn(colors = colorRampPalette(rev(RColorBrewer::brewer.pal(11, "RdBu")))(255),
+                             limits = c(-5,5),
+                             na.value = "transparent",
+                             name = "Anomaly °C") +
+        coord_equal() +
+        theme_classic() +
+        scale_x_continuous(breaks = seq(-113,-105.5, 1.0),
+                           limits = c(-113,-105.5)) +
+        scale_y_continuous(breaks = seq(33.5, 38.0, 0.5),
+                           limits = c(33.5, 38.0)) +
+        geom_sf(
+          data = usa,
+          color = "#2b2b2b",
+          fill = "transparent",
+          size = 0.5
+        ) +
+        xlab("Longitude") +
+        ylab("Latitude") +
+        ggtitle(title.name) +
+        theme(
+          panel.border = element_blank(),
+          panel.grid.major = element_blank(),
+          axis.text = element_text(
+            size = 12,
+            colour = "black",
+            family = "Helvetica"
+          ),
+          axis.title.y = element_text(
+            size = 20,
+            family = "Helvetica",
+            margin = margin(
+              t = 10,
+              r = 10,
+              b = 10,
+              l = 10
+            )
+          ),
+          axis.title.x = element_text(
+            size = 20,
+            family = "Helvetica",
+            margin = margin(
+              t = 10,
+              r = 10,
+              b = 10,
+              l = 10
+            )
+          ),
+          plot.title = element_text(
+            hjust = 0.5,
+            size = 24,
+            family = "Helvetica"
+          ),
+          panel.grid.minor = element_line(colour = "light grey"),
+          axis.line = element_line(colour = "black"),
+          legend.text = element_text(size = 13, family = "Helvetica"),
+          legend.title = element_text(size = 15, family = "Helvetica")
+        )
+
+    } else {
+
+      anomaly_plots <- ggplot() +
+        geom_raster(
+          data = temperature_transformed,
+          aes(x = x, y = y, fill = var1.pred),
+          alpha = 0.8,
+          na.rm = TRUE
+        ) +
+        scale_fill_gradientn(colors = colorRampPalette(rev(RColorBrewer::brewer.pal(11, "RdBu")))(255),
+                             limits = c(-8,8),
+                             na.value = "transparent",
+                             name = "Anomaly °C") +
+        coord_equal() +
+        theme_classic() +
+        scale_x_continuous(breaks = seq(-113,-105.5, 1.0),
+                           limits = c(-113,-105.5)) +
+        scale_y_continuous(breaks = seq(33.5, 38.0, 0.5),
+                           limits = c(33.5, 38.0)) +
+        geom_sf(
+          data = usa,
+          color = "#2b2b2b",
+          fill = "transparent",
+          size = 0.5
+        ) +
+        geom_sf(
+          data = cities,
+          shape = 23,
+          fill = "blue",
+          size = 3
+        ) +
+        shadowtext::geom_shadowtext(
+          data = cities2,
+          aes(x = long, y = lat, label = name),
+          nudge_y = 0.15,
+          color = "white"
+        ) +
+        xlab("Longitude") +
+        ylab("Latitude") +
+        ggtitle(title.name) +
+        theme(
+          panel.border = element_blank(),
+          panel.grid.major = element_blank(),
+          axis.text = element_text(
+            size = 12,
+            colour = "black",
+            family = "Helvetica"
+          ),
+          axis.title.y = element_text(
+            size = 20,
+            family = "Helvetica",
+            margin = margin(
+              t = 10,
+              r = 10,
+              b = 10,
+              l = 10
+            )
+          ),
+          axis.title.x = element_text(
+            size = 20,
+            family = "Helvetica",
+            margin = margin(
+              t = 10,
+              r = 10,
+              b = 10,
+              l = 10
+            )
+          ),
+          plot.title = element_text(
+            hjust = 0.5,
+            size = 24,
+            family = "Helvetica"
+          ),
+          panel.grid.minor = element_line(colour = "light grey"),
+          axis.line = element_line(colour = "black"),
+          legend.text = element_text(size = 13, family = "Helvetica"),
+          legend.title = element_text(size = 15, family = "Helvetica")
+        )
+
+
+    }
+
     anomaly_plots <- ggplot() +
       geom_raster(
         data = temperature_transformed,
-        aes(x = x, y = y, fill = layer),
+        aes(x = x, y = y, fill = var1.pred),
         alpha = 0.8,
         na.rm = TRUE
       ) +
