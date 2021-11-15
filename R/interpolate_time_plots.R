f6cd12abba78cf4dfd9eaa3942ccb12350d796e0
Andrew Gillreath-Brown <andrew.brown1234@gmail.com>
2021-11-15 15:19:05 -0800

    Added individual site reconstructions for raw temps and anomaly.

--- R/interpolate_time_plots.R
+++ R/interpolate_time_plots.R
@@ -42,18 +42,19 @@ interpolate_time_plots <-
             multiple = 0.5,
             FUN = ceiling
           )
-        ), 0.5)) +
+        ), 1.0)) +
         theme_bw() +
         theme(
           panel.border = element_blank(),
           panel.grid.major = element_blank(),
+          plot.margin = margin(5.0, 5.0, 5.0, 5.0),
           axis.text = element_text(
-            size = 14,
+            size = 10,
             colour = "black",
             family = "Helvetica"
           ),
           axis.title.y = element_text(
-            size = 20,
+            size = 12,
             family = "Helvetica",
             margin = margin(
               t = 10,
@@ -63,7 +64,7 @@ interpolate_time_plots <-
             )
           ),
           axis.title.x = element_text(
-            size = 20,
+            size = 12,
             family = "Helvetica",
             margin = margin(
               t = 20,
@@ -74,8 +75,8 @@ interpolate_time_plots <-
           ),
           panel.grid.minor = element_line(colour = "light grey"),
           axis.line = element_line(colour = "black"),
-          legend.text = element_text(size = 18, family = "Helvetica"),
-          legend.title = element_text(size = 22, family = "Helvetica")
+          legend.text = element_text(size = 10, family = "Helvetica"),
+          legend.title = element_text(size = 11, family = "Helvetica")
         )
 
     } else {
@@ -102,13 +103,14 @@ interpolate_time_plots <-
         theme(
           panel.border = element_blank(),
           panel.grid.major = element_blank(),
+          plot.margin = margin(5.0, 5.0, 5.0, 5.0),
           axis.text = element_text(
-            size = 14,
+            size = 10,
             colour = "black",
             family = "Helvetica"
           ),
           axis.title.y = element_text(
-            size = 20,
+            size = 12,
             family = "Helvetica",
             margin = margin(
               t = 10,
@@ -118,7 +120,7 @@ interpolate_time_plots <-
             )
           ),
           axis.title.x = element_text(
-            size = 20,
+            size = 12,
             family = "Helvetica",
             margin = margin(
               t = 20,
@@ -129,8 +131,8 @@ interpolate_time_plots <-
           ),
           panel.grid.minor = element_line(colour = "light grey"),
           axis.line = element_line(colour = "black"),
-          legend.text = element_text(size = 18, family = "Helvetica"),
-          legend.title = element_text(size = 22, family = "Helvetica")
+          legend.text = element_text(size = 10, family = "Helvetica"),
+          legend.title = element_text(size = 11, family = "Helvetica")
         )
     }
   }
