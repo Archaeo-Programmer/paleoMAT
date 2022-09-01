4d3ff47a72057bb8d296fc5ff78a75b4eeb1bce4
Andrew Gillreath-Brown <andrew.brown1234@gmail.com>
2022-09-01 11:13:57 -0700

    Added data.R file for documentation for data.

---
+++ R/data.R
@@ -0,0 +1,33 @@
+#' PRISM Climate for July Dataset
+#'
+#' The fossil and modern pollen datasets were originally downloaded from the
+#' Neotoma Paleoecology Database. We processed and cleaned those data in
+#' UUSS_MAT_Reconstruction.Rmd. The climate data was acquired from PRISM.
+#'
+#' @format An object of class `tibble`.
+#' @source \url{https://prism.oregonstate.edu/}
+"prism_7"
+
+#' Fossil Pollen dataset for SWUS across all time.
+#'
+#' A `tibble` containing proportional fossil pollen for the SWUS.
+#'
+#' @format An object of class `tibble`.
+#' @source \url{https://www.neotomadb.org/}
+"fossil_pollen"
+
+#' Fossil Pollen dataset for SWUS for post 5500 BP.
+#'
+#' Fossil Pollen dataset for SWUS for post 5500 BP and that have ages for each sample.
+#'
+#' @format An object of class `tibble`.
+#' @source \url{https://www.neotomadb.org/}
+"fossil_west_post5500"
+
+#' Modern Pollen dataset for western United States.
+#'
+#' A `tibble` containing proportional modern pollen for western United States.
+#'
+#' @format An object of class `tibble`.
+#' @source \urlhttps://www.neotomadb.org/}
+"MPCT_proportions"
